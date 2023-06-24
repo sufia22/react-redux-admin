3cb4f65c3732ed3d3a2fb299420057d87ef3cd73
Sufia <shapu23@gmail.com>
2023-06-24 13:09:01 +0600

    Import LoginTemplate

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,5 +1,66 @@
+import logoWhite from "../../assets/img/logo-white.png";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <div className="main-wrapper login-body">
+        <div className="login-wrapper">
+          <div className="container">
+            <div className="loginbox">
+              <div className="login-left">
+                <img className="img-fluid" src={logoWhite} alt="Logo" />
+              </div>
+              <div className="login-right">
+                <div className="login-right-wrap">
+                  <h1>Login</h1>
+                  <p className="account-subtitle">Access to our dashboard</p>
+
+                  {/* <!-- Form --> */}
+                  <form action="https://dreamguys.co.in/demo/doccure/admin/index.html">
+                    <div className="form-group">
+                      <input
+                        className="form-control"
+                        type="text"
+                        placeholder="Email"
+                      />
+                    </div>
+                    <div className="form-group">
+                      <input
+                        className="form-control"
+                        type="text"
+                        placeholder="Password"
+                      />
+                    </div>
+                    <div className="form-group">
+                      <button
+                        className="btn btn-primary btn-block"
+                        type="submit"
+                      >
+                        Login
+                      </button>
+                    </div>
+                  </form>
+                  {/* <!-- /Form --> */}
+
+                  <div className="text-center forgotpass">
+                    <a href="forgot-password.html">Forgot Password?</a>
+                  </div>
+                  <div className="login-or">
+                    <span className="or-line"></span>
+                    <span className="span-or">or</span>
+                  </div>
+
+                  <div className="text-center dont-have">
+                    Don’t have an account? <a href="register.html">Register</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
